2cd192ba8886100bad2c260c99041ee07cf58905
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-08-19 14:43:45 +0200

    step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { WikipediaSearchService } from './wikipedia-search.service';
-
+import { Subject } from 'rxjs/Subject';
 //application wide shared Rx operators
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   moduleId: module.id,
@@ -12,7 +14,13 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   items:Array<string>;
-  constructor(private service:WikipediaSearchService) {}
+  term$ = new Subject<string>();
+  constructor(private service:WikipediaSearchService) {
+    this.term$
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .subscribe(term => this.search(term));
+  }
 
   search(term: string) {
     this.service.search(term)
